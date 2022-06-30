e17e9ec42c399cb744109df6f62b0ea640c30698
Jaime <79403778+Jaimepas77@users.noreply.github.com>
2022-06-30 23:37:00 +0200

    Restarting the start
    A batch controlled board is implemented. It supports switching on and off any square one by one.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,25 +1,74 @@
 package controller;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 import model.Game;
 
 public class Controller {
 	public static final int DEFAULT_ROWS = 6;
 	public static final int DEFAULT_COLLUMNS = 6;
-	
+
 	//the game itself
 	private static Game game;
-	
+
+	private static boolean exit = false;
+	private static Scanner in = new Scanner(System.in);
 	//the interface (printline style class)
 
 	public static void main(String[] args) {
 		//Choose the interface type (batch - GUI)
-		
+
 		//Create game instance
 		game = new Game(DEFAULT_ROWS, DEFAULT_COLLUMNS);
-		
+
 		//Run interface thread (passing the controller itself as a parameter)
-		
-		
+
+
+
+		while(!exit) {
+			System.out.println(game.getBoard()); // refreshBoard
+
+			try {
+				String[] command = getCommand();
+				executeCommand(command);
+			}
+			catch(IllegalArgumentException e) {
+				System.err.println("Comando no soportado");
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static String[] getCommand() {
+		System.out.print(">");
+		String ret[] = in.nextLine().split(" ");
+		return ret;
+	}
+
+	private static void executeCommand(String[] args) throws IllegalArgumentException {
+		//Los comandos se añaden aquí
+		switch(args[0]) {
+		case "on":
+			game.setSquareState(true, Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+			break;
+		case "off":
+			game.setSquareState(false, Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+			break;
+		case "exit":
+			exit = true;
+			break;
+		case "help":
+			System.out.println("Helping...");
+			System.out.println("on <x> <y>: switch on square in the row x and column y");
+			System.out.println("off <x> <y>: switch off square in the row x and column y");
+			System.out.println("exit: finish the program");
+			break;
+		default:
+			throw new IllegalArgumentException();
+		}
 	}
 
 }
--- src/model/Game.java
+++ src/model/Game.java
@@ -5,20 +5,42 @@ public class Game {
 	//	x	,	y
 	
 	//Board
+	private boolean board[][];
 	
 	public Game(int rows, int columns)
 	{
 		this.rows = rows;
 		this.columns = columns;
+		board = new boolean[rows][columns];//Inicializado a false
 	}
 
 	public void setSquareState(boolean state, int x, int y)	//Set the square to on or off
 	{
 		//Board.setSquareState(state, x, y);
+		board[x][y] = state;
 	}
 	
-	public void getSquareState(int x, int y)
+	public boolean getSquareState(int x, int y)
 	{
 		//Board.getSquareState(x, y);
+		return board[x][y];
+	}
+
+	public String getBoard() {
+		String ret = "";
+		
+		for(boolean[] r : board) {
+			for(boolean x : r) {
+				if(x) {
+					ret += 'O';
+				}
+				else {
+					ret += '_';
+				}
+			}
+			ret += '\n';
+		}
+		
+		return ret;
 	}
 }
