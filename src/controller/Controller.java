24154e92ddd309936e9a5963513019581188c556
Abbissa <carlospozoserrano13@gmail.com>
2022-07-07 11:46:39 +0200

    Intentos de mejora sustanciales, ahora no funciona
    Es porque no hay botones y no hay forma de pintar, luego se mira

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -10,19 +10,68 @@ public class Controller {
 	public static final int DEFAULT_ROWS = 30;
 	public static final int DEFAULT_COLUMNS = 40;
 
-	//the game itself
+	// the game itself
 	private static Game game;
 
 	public static void main(String[] args) {
-		//Create game instance
-		game = new Game(DEFAULT_ROWS, DEFAULT_COLUMNS);
+		// Create game instance
+
+		// Choose the interface type (batch - GUI) and run it
+
 		
-		//Choose the interface type (batch - GUI) and run it
-		int op = JOptionPane.showConfirmDialog(null, "¿Quieres emplear la interfaz gráfica?", "Elegir interfaz", JOptionPane.YES_NO_OPTION);
-		if(op == JOptionPane.YES_OPTION) {
-			new GraphicUI(game);
+		boolean invalid = true;
+		int rows = DEFAULT_ROWS;
+		int columns = DEFAULT_COLUMNS;
+
+		while (invalid) {
+			String int1 = JOptionPane.showInputDialog("Introduzca numero de filas");
+			try {
+				invalid = false;
+				rows = Integer.parseInt(int1);
+			} catch (NumberFormatException e) {
+				JOptionPane.showMessageDialog(null, "El texto " + int1 + " no se corresponde con un numero");
+
+				invalid = true;
+				continue;
+			}
 		}
-		else {
+
+		if (rows <= 0) {
+			JOptionPane.showMessageDialog(null,
+					rows + " menor que 0, sustituido por valor por defecto: " + DEFAULT_ROWS);
+			rows = DEFAULT_ROWS;
+		}
+		invalid = true;
+
+		while (invalid) {
+			String int1 = JOptionPane.showInputDialog("Introduzca numero de columnas");
+			try {
+				invalid = false;
+				columns = Integer.parseInt(int1);
+			} catch (NumberFormatException e) {
+				JOptionPane.showMessageDialog(null, "El texto " + int1 + " no se corresponde con un numero");
+				invalid = true;
+				continue;
+			}
+		}
+
+		if (columns <= 0) {
+			JOptionPane.showMessageDialog(null,
+					columns + " menor que 0, sustituido por valor por defecto: " + DEFAULT_ROWS);
+			columns = DEFAULT_COLUMNS;
+		}
+		game = new Game(rows, columns);
+
+		int op = JOptionPane.showConfirmDialog(null, "¿Quieres emplear la interfaz gráfica?", "Elegir interfaz",
+				JOptionPane.YES_NO_OPTION);
+				
+		if (op == JOptionPane.YES_OPTION) {
+
+
+			new GraphicUI(game);
+		} else
+
+		{
 			new BatchUI(game);
 		}
 	}
--- src/view/GUIresources/BoardGrid.java
+++ src/view/GUIresources/BoardGrid.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.Graphics;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -18,7 +19,7 @@ public class BoardGrid extends JPanel implements GameObserver, ToolBarObserver {
 	private static final long serialVersionUID = 1L;
 
 	private Game game;
-	private JButton[][] boardBoxes;// Boxes of the board
+	private RedSquare[][] boardBoxes;// Boxes of the board
 	private int numRows;
 	private int numCols;
 	private Color aliveColor = Color.YELLOW;
@@ -37,62 +38,69 @@ public class BoardGrid extends JPanel implements GameObserver, ToolBarObserver {
 	public void initBoard() {
 		this.setLayout(new GridLayout(numRows, numCols, -1, -1));// -1 of gap in order to eliminate the gap that exists
 																	// with 0 as the parameter
-
-		boardBoxes = new JButton[numRows][numCols];
+		boardBoxes = new RedSquare[numRows][numCols];
 
 		for (int i = 0; i < numRows; i++) {
 			for (int j = 0; j < numCols; j++) {
 				// Initialising button properties
-				boardBoxes[i][j] = new JButton();
-				boardBoxes[i][j].setBackground(deadColor);
-				boardBoxes[i][j].setBorderPainted(true);
+				boardBoxes[i][j] = new RedSquare(i, j, 10, 10, aliveColor);
 
 				// button actions configuration
 				int x = i;// A separate variable to preserve the final coordinates for the button
 				int y = j;
-				boardBoxes[i][j].addActionListener(new ActionListener() {// Click
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						if (game.getSelectionState() == 1) {
-							game.selectFirst(x, y);
-						} else if (game.getSelectionState() == 2) {
-							game.selectSecond(x, y);
-						} else if (game.getSelectionState() == 3) {
-							game.insertToBeInserted(x, y);// In fact, copy the selected area to the clicked place
-						} else
-							game.setSquareState(!game.getSquareState(x, y), x, y);// Invert squares state
 
-					}
-				});
+				
+				/*
+				 * boardBoxes[i][j].addActionListener(new ActionListener() {// Click
+				 * 
+				 * @Override
+				 * public void actionPerformed(ActionEvent e) {
+				 * if (game.getSelectionState() == 1) {
+				 * game.selectFirst(x, y);
+				 * } else if (game.getSelectionState() == 2) {
+				 * game.selectSecond(x, y);
+				 * } else if (game.getSelectionState() == 3) {
+				 * game.insertToBeInserted(x, y);// In fact, copy the selected area to the
+				 * clicked place
+				 * } else
+				 * game.setSquareState(!game.getSquareState(x, y), x, y);// Invert squares state
+				 * 
+				 * }
+				 * });
+				 * 
+				 * boardBoxes[i][j].addMouseListener(new MouseAdapter() {
+				 * 
+				 * @Override
+				 * public void mouseEntered(MouseEvent e) {
+				 * if (game.isToBeInserted()) {
+				 * boolean[][] tmp = game.getToBeInserted();
+				 * for (int k = x; k - x < tmp.length && k < numRows; k++) {
+				 * for (int l = y; l - y < tmp[k - x].length && l < numCols; l++) {
+				 * if (tmp[k - x][l - y]) {
+				 * boardBoxes[k][l].setColor(aliveColor.darker());
+				 * } else {
+				 * boardBoxes[k][l].setColor(deadColor.darker());// Paint the structure in a
+				 * // darker color
+				 * }
+				 * }
+				 * }
+				 * }
+				 * }
+				 */
 
-				boardBoxes[i][j].addMouseListener(new MouseAdapter() {
-					@Override
-					public void mouseEntered(MouseEvent e) {
-						if (game.isToBeInserted()) {
-							boolean[][] tmp = game.getToBeInserted();
-							for (int k = x; k - x < tmp.length && k < numRows; k++) {
-								for (int l = y; l - y < tmp[k - x].length && l < numCols; l++) {
-									if (tmp[k - x][l - y]) {
-										boardBoxes[k][l].setBackground(aliveColor.darker());
-									} else {
-										boardBoxes[k][l].setBackground(deadColor.darker());// Paint the structure in a
-																							// darker color
-									}
-								}
-							}
-						}
-					}
-
-					@Override
-					public void mouseExited(MouseEvent e) {
-						if (game.isToBeInserted()) {
-							onBoardUpdate();// Repaint the board unpainting the previsualization of the insertion
-						}
-					}
-				});
+				/*
+				 * @Override
+				 * public void mouseExited(MouseEvent e) {
+				 * if (game.isToBeInserted()) {
+				 * onBoardUpdate();// Repaint the board unpainting the previsualization of the
+				 * insertion
+				 * }
+				 * }
+				 * });
+				 */
 
 				// Adding the button to the panel
-				this.add(boardBoxes[i][j]);
+				this.repaint();
 			}
 		}
 	}
@@ -104,18 +112,18 @@ public class BoardGrid extends JPanel implements GameObserver, ToolBarObserver {
 		for (int i = 0; i < numRows; i++) {
 			for (int j = 0; j < numCols; j++) {
 				if (board[i][j]) {
-					if (boardBoxes[i][j].getBackground() != aliveColor) {
-						boardBoxes[i][j].setBackground(aliveColor);
+					if (boardBoxes[i][j].getColor() != aliveColor) {
+						boardBoxes[i][j].setColor(aliveColor);
 					}
 				} else {
-					if (boardBoxes[i][j].getBackground() != deadColor) {// Only act if it is necessary (to prevent lag)
-						boardBoxes[i][j].setBackground(deadColor);
+					if (boardBoxes[i][j].getColor() != deadColor) {// Only act if it is necessary (to prevent lag)
+						boardBoxes[i][j].setColor(deadColor);
 					}
 				}
 
 				if (game.getSelectionState() == 2) {
 					if (i == game.getX1() && j == game.getY1()) {
-						boardBoxes[i][j].setBackground(boardBoxes[i][j].getBackground().darker());
+						boardBoxes[i][j].setColor(boardBoxes[i][j].getColor().darker());
 					}
 				} else if (game.getSelectionState() == 3) {
 					int x1 = game.getX1();
@@ -124,7 +132,7 @@ public class BoardGrid extends JPanel implements GameObserver, ToolBarObserver {
 					int y2 = game.getY2();
 					if (i >= Math.min(x1, x2) && i <= Math.max(x1, x2) && j >= Math.min(y1, y2)
 							&& j <= Math.max(y1, y2)) {
-						boardBoxes[i][j].setBackground(boardBoxes[i][j].getBackground().darker());
+						boardBoxes[i][j].setColor(boardBoxes[i][j].getColor().darker());
 					}
 				}
 			}
@@ -141,5 +149,61 @@ public class BoardGrid extends JPanel implements GameObserver, ToolBarObserver {
 	public void onSecondColorUpdate(Color c) {
 		deadColor = c;
 		onBoardUpdate();
+	}
+}
+
+class RedSquare {
+
+	private int xPos;
+	private int yPos;
+	private int width;
+	private int height;
+	private Color color;
+
+	public RedSquare(int xPos, int yPos, int width, int height, Color color) {
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.width = width;
+		this.height = height;
+		this.color = color;
+	}
+
+	public void setX(int xPos) {
+		this.xPos = xPos;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return this.color;
+	}
+
+	public int getX() {
+		return xPos;
+	}
+
+	public void setY(int yPos) {
+		this.yPos = yPos;
+	}
+
+	public int getY() {
+		return yPos;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void paintSquare(Graphics g) {
+		g.setColor(color);
+		g.fillRect(xPos, yPos, width, height);
+		g.setColor(Color.BLACK);
+		g.drawRect(xPos, yPos, width, height);
 	}
 }
