532667bb0b252f51bc95499434e28497f99efb74
Jaime <79403778+Jaimepas77@users.noreply.github.com>
2022-08-12 16:29:23 +0200

    Click improvements
    Now you can't click in a square while the game is running steps. Also a few ideas were added.

--- src/view/GUIresources/BoardUI.java
+++ src/view/GUIresources/BoardUI.java
@@ -17,6 +17,7 @@ public class BoardUI extends JPanel implements GameObserver, ToolBarObserver {
     private Color aliveColor = Color.YELLOW;
     private Color deadColor = Color.WHITE;
     private int mouseX = -1, mouseY = -1;// -1 means that the mouse is out of the board. x - collumns ; y - rows
+    private boolean enableClick = true;
 
     public BoardUI(Game game) {
         super();
@@ -48,30 +49,40 @@ public class BoardUI extends JPanel implements GameObserver, ToolBarObserver {
                 int y = (int) resizeCoord(e.getY(), getHeight(), numRows);
                 overCell(y, x);
             }
+
+            public void mouseDragged(MouseEvent e) {
+                int x = (int) resizeCoord(e.getX(), getWidth(), numCols);
+                int y = (int) resizeCoord(e.getY(), getHeight(), numRows);
+                overCell(y, x);
+            }
         });
     }
 
-    private float resizeCoord(float x, float maxX, float newMaxX) {//Resize coordinates without losing precision
-        float newX = x/maxX * newMaxX;
+    private float resizeCoord(float x, float maxX, float newMaxX) {// Resize coordinates without losing precision
+        float newX = x / maxX * newMaxX;
         return newX;
     }
 
     private void clickCell(int row, int col) {
-        if (game.getSelectionState() == 1) {
-            game.selectFirst(row, col);
-        } else if (game.getSelectionState() == 2) {
-            game.selectSecond(row, col);
-        } else if (game.getSelectionState() == 3 || game.isToBeInserted()) {
-            game.insertToBeInserted(row, col);// In fact, copy the selected area to the clicked place
-        } else {
-            game.setSquareState(!game.getSquareState(row, col), row, col);// Invert squares state
+        if (enableClick) {//Only do smth when the board click is enabled
+            if (game.getSelectionState() == 1) {
+                game.selectFirst(row, col);
+            } else if (game.getSelectionState() == 2) {
+                game.selectSecond(row, col);
+            } else if (game.getSelectionState() == 3 || game.isToBeInserted()) {
+                game.insertToBeInserted(row, col);// In fact, copy the selected area to the clicked place
+            } else {
+                game.setSquareState(!game.getSquareState(row, col), row, col);// Invert squares state
+            }
         }
     }
 
-    private void overCell(int row, int col) {
+private void overCell(int row, int col) {
         mouseX = col;
         mouseY = row;
-        repaint();
+        if (enableClick) {//Only paint the mouse pos when you can do smth with it
+            repaint();
+        }
     }
 
     protected void paintComponent(Graphics g) {
@@ -86,36 +97,37 @@ public class BoardUI extends JPanel implements GameObserver, ToolBarObserver {
                 // Properties of the square to be painted
                 int x = (int) resizeCoord(j, numCols, w);
                 int y = (int) resizeCoord(i, numRows, h);
-                int width = (int) resizeCoord(j+1, numCols, w) - x;//The space between the next col and this one is the width
-                int height = (int) resizeCoord(i+1, numRows, h) - y;
+                int width = (int) resizeCoord(j + 1, numCols, w) - x;// The space between the next col and this one is
+                                                                     // the width
+                int height = (int) resizeCoord(i + 1, numRows, h) - y;
 
                 if (board[i][j]) {
                     g.setColor(aliveColor);
-                } 
-                else {
+                } else {
                     g.setColor(deadColor);
                 }
 
-				if(game.getSelectionState() == 2) {
-					if(i == game.getX1() && j == game.getY1()) {
+                if (game.getSelectionState() == 2) {
+                    if (i == game.getX1() && j == game.getY1()) {
                         g.setColor(g.getColor().darker());
-					}
-				}
-				else if(game.getSelectionState() == 3) {
-					int x1 = game.getX1();
-					int y1 = game.getY1();
-					int x2 = game.getX2();
-					int y2 = game.getY2();
-					if(i >= Math.min(x1, x2) && i <= Math.max(x1, x2) && j >= Math.min(y1,  y2) && j <= Math.max(y1,  y2)) {//If this square is in the range of the selected values ...
+                    }
+                } else if (game.getSelectionState() == 3) {
+                    int x1 = game.getX1();
+                    int y1 = game.getY1();
+                    int x2 = game.getX2();
+                    int y2 = game.getY2();
+                    if (i >= Math.min(x1, x2) && i <= Math.max(x1, x2) && j >= Math.min(y1, y2)
+                            && j <= Math.max(y1, y2)) {// If this square is in the range of the selected values ...
                         g.setColor(g.getColor().darker());
-					}
-				}
+                    }
+                }
 
-                if (mouseY != -1 && (game.isToBeInserted() || game.getSelectionState() == 3) && i >= mouseY && j >= mouseX) {//To paint a preview of a potential insertion
+                if (mouseY != -1 && (game.isToBeInserted() || game.getSelectionState() == 3) && i >= mouseY
+                        && j >= mouseX) {// To paint a preview of a potential insertion
                     boolean[][] tmp = game.getToBeInserted();
-                    if(tmp.length > i - mouseY && tmp[0].length > j - mouseX) {
+                    if (tmp.length > i - mouseY && tmp[0].length > j - mouseX) {
                         // Paint the structure in a darker color
-                        if (tmp[i-mouseY][j-mouseX]) {
+                        if (tmp[i - mouseY][j - mouseX]) {
                             g.setColor(aliveColor.darker());
                         } else {
                             g.setColor(deadColor.darker());
@@ -130,12 +142,19 @@ public class BoardUI extends JPanel implements GameObserver, ToolBarObserver {
                 g.fillRect(x, y, width, height);
                 g.setColor(Color.BLACK);
                 g.drawRect(x, y, width, height);
-                if (mouseY == i && mouseX == j) {
+                if (enableClick && mouseY == i && mouseX == j) {
                     g.setColor(Color.ORANGE);
                     g.drawRect(x + 1, y + 1, width - 2, height - 2);
                 }
             }
         }
+    }
+
+    @Override
+    public void onRunningUpdate(boolean running) {
+        enableClick = !running;// Only enable the board interactions when the game is not running
+
+        repaint();
     }
 
     @Override
